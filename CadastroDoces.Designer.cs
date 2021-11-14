a40b64182a81340670928def732fbd0af28c2692
Ricardo Sousa <ricksantos.rs25@gmail.com>
2021-11-13 21:17:33 -0300

    Telas do produto

---
+++ CadastroDoces.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Confeitaria
+{
+    partial class CadastroDoces
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CadastroDoces));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbldataVenc = new System.Windows.Forms.Label();
+            this.lblDataFab = new System.Windows.Forms.Label();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.lblQtd = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label1.Location = new System.Drawing.Point(45, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 40);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Cadastrar";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(543, 234);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(80, 23);
+            this.dateTimePicker2.TabIndex = 35;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(543, 171);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(80, 23);
+            this.dateTimePicker1.TabIndex = 34;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(175, 290);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(448, 23);
+            this.textBox4.TabIndex = 33;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(175, 234);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 32;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 171);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 31;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(175, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 23);
+            this.textBox1.TabIndex = 30;
+            // 
+            // lbldataVenc
+            // 
+            this.lbldataVenc.AutoSize = true;
+            this.lbldataVenc.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbldataVenc.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lbldataVenc.Location = new System.Drawing.Point(320, 230);
+            this.lbldataVenc.Name = "lbldataVenc";
+            this.lbldataVenc.Size = new System.Drawing.Size(207, 30);
+            this.lbldataVenc.TabIndex = 29;
+            this.lbldataVenc.Text = "Data de Vencimento:";
+            // 
+            // lblDataFab
+            // 
+            this.lblDataFab.AutoSize = true;
+            this.lblDataFab.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDataFab.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblDataFab.Location = new System.Drawing.Point(320, 165);
+            this.lblDataFab.Name = "lblDataFab";
+            this.lblDataFab.Size = new System.Drawing.Size(195, 30);
+            this.lblDataFab.TabIndex = 28;
+            this.lblDataFab.Text = "Data de Fabricação:";
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDescricao.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblDescricao.Location = new System.Drawing.Point(45, 283);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(108, 30);
+            this.lblDescricao.TabIndex = 27;
+            this.lblDescricao.Text = "Descrição:";
+            // 
+            // lblQtd
+            // 
+            this.lblQtd.AutoSize = true;
+            this.lblQtd.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblQtd.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblQtd.Location = new System.Drawing.Point(45, 230);
+            this.lblQtd.Name = "lblQtd";
+            this.lblQtd.Size = new System.Drawing.Size(127, 30);
+            this.lblQtd.TabIndex = 26;
+            this.lblQtd.Text = "Quantidade:";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPreco.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblPreco.Location = new System.Drawing.Point(45, 171);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(70, 30);
+            this.lblPreco.TabIndex = 25;
+            this.lblPreco.Text = "Preço:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lblNome.Location = new System.Drawing.Point(45, 111);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(75, 30);
+            this.lblNome.TabIndex = 24;
+            this.lblNome.Text = "Nome:";
+            // 
+            // CadastroDoces
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 369);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbldataVenc);
+            this.Controls.Add(this.lblDataFab);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.lblQtd);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CadastroDoces";
+            this.Text = "CadastroDoces";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbldataVenc;
+        private System.Windows.Forms.Label lblDataFab;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Label lblQtd;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.Label lblNome;
+    }
+}
